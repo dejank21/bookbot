92873c49b11e3e315d73d1f46cd26bea2b72b0c7
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-10-18 17:40:32 -0500

    added code to create a get book function

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as file:
+        file_contents = file.read()
+    return file_contents
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+main() 
