027e89d312ba786b19b17bba718516657bfcef2b
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-10-20 17:48:24 -0500

    Removed git ignore

--- stats.py
+++ stats.py
@@ -4,11 +4,11 @@ def get_book_text(path_to_file):
     return file_contents
 
 def get_num_words(file_contents):
-    file_contents = file_contents.split()
-    print(f"Found {len(file_contents)} total words")
+    num_words = file_contents.split()
+    print(f"Found {len(num_words)} total words")
 
-def times_each_character(file_contents):
-    char_list = file_contents.split().lower()
+def times_each_character(get_book_text):
+    char_list =  get_book_text.split().lower()  
     char_count = {}
     for char in char_list:
         if char in char_count:
