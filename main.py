9f735edc391b3c9c86c35a5446292ec2c8ba38e5
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-10-21 12:39:02 -0500

    Added two book and sys functionality to take book path as an arg

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from stats import times_each_character, get_book_text, get_num_words, characters
 import sys 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     char_to_sort = times_each_character(text)
     num_words = get_num_words(text)
     sorted_characters = characters_sort(char_to_sort)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("------- Character Count ---------")
